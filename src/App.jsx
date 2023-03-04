497312862d5a67d73e812bdf8e5a5144777ac4b3
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-04 11:00:52 +0630

    relax

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,26 @@ import {
   StarsCanvas,
 } from "./components";
 
-const App = () => (
-  <BrowserRouter>
-    <div>abc</div>
-  </BrowserRouter>
-);
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
