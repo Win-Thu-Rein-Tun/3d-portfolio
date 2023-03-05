cf428f55f28fd83123b91e3124e2087ac7308669
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-05 15:23:54 +0630

    complete tech

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.heroSubText}>Introduction</p>
-        <h2 className={styles.heroHeadText}>Overview</h2>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
 
       <motion.p
@@ -50,7 +50,9 @@ const ServiceCard = ({ index, title, icon }) => {
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
